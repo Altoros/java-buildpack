bb1fe228ee703ab0601e2ec8a4f0879504690e51
ykisialiou <hiluro@gmail.com>
2018-04-24 22:12:40 +0300

    Possibly hotfix to jar path

--- spec/java_buildpack/framework/jmxtrans_agent_spec.rb
+++ spec/java_buildpack/framework/jmxtrans_agent_spec.rb
@@ -87,7 +87,7 @@ describe JavaBuildpack::Framework::JmxtransAgent do
             expect(java_opts).to include('-Dgraphite.port=0000')
             expect(java_opts).to include('-Dgraphite.prefix=test-prefix.test-application-name.${CF_INSTANCE_INDEX}')
             precompiled = '-javaagent:' \
-              "$PWD/.java-buildpack/jmxtrans_agent/jmxtrans_agent-#{version}.jar=" \
+              "$PWD/.java-buildpack/jmxtrans_agent/jmxtrans-agent-#{version}.jar=" \
               '$PWD/.java-buildpack/jmxtrans_agent/jmxtrans-agent.xml'
             expect(java_opts).to include(precompiled)
           end
